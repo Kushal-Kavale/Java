83f6ba5d12f2fc66d2116e925caae8fea8821b30
Kushal-Kavale <kushalkavale18@gmail.com>
2026-07-14 11:13:39 +0530

    Add Practice.java for addition of java

---
+++ Practice.java
@@ -0,0 +1,14 @@
+
+public class Practice{
+   public static void main(String[] args){
+       int num1 = 30;
+        int num2 = 66;
+        
+        int sum = num1 + num2;
+        
+        System.out.println("Number1 :"+num1);
+        System.out.println("Number2 :"+num2);
+        System.out.println("Addition of "+num1+"And"+num2+"is"+sum);
+       
+   }  
+}
